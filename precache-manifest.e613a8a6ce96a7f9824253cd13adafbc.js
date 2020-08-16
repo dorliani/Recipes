5bfab04c89a9299466f6f5802aee4917cc253bce
Dor Liani <ddorliani@gmail.com>
2020-08-16 17:51:43 +0300

    Updates

---
+++ precache-manifest.e613a8a6ce96a7f9824253cd13adafbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76c213e0006bf455060a8847d783a7b6",
+    "url": "/Yummly/index.html"
+  },
+  {
+    "revision": "b84a3b36535615ce014a",
+    "url": "/Yummly/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4ec6cd2f72528286cb73",
+    "url": "/Yummly/static/css/main.c30009fa.chunk.css"
+  },
+  {
+    "revision": "b84a3b36535615ce014a",
+    "url": "/Yummly/static/js/2.cf2ce6e6.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Yummly/static/js/2.cf2ce6e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec6cd2f72528286cb73",
+    "url": "/Yummly/static/js/main.3eb7a4e0.chunk.js"
+  },
+  {
+    "revision": "c2107a7a2c023e59a474",
+    "url": "/Yummly/static/js/runtime-main.d955bece.js"
+  }
+]);
